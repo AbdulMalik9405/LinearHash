dd4bd02dc93c1b1a38ebe49885f715f99da3ccc1
AbdulMalik9405 <abdulmaliktojahmedov9405@gmail.com>
2025-09-07 15:43:44 +0900

    Added functions and removed extra lines of code

--- LinearHashing.py
+++ LinearHashing.py
@@ -3,70 +3,51 @@ import statistics
 
 Students = ["Malik", "Abdel", "Josh", "Aziz", "Thomas", " Karl", "James", "Eric", "Jason", "David", "Ron", "Bob", "Charlie", "Alice", "Lily", "Mason", "Alex", 
             "Noah", "Olivia", "Emma", "Ella", "Tom", "Max"]
-
-StudentsInt = []
-
-StudentsIntHash = []
-
-HashTable = [
-    [],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
-
+StudentsHash = []
+HashTable = [[] for _ in range(20)]
 times = []
 
-name = input("Enter a name: ")
-for j in range(100):
-    start = time.perf_counter()
-
+def linear_search(name):
     found = True
-
     for i in range(len(Students)):
         if Students[i] == name:
             print(name, " found at position", i+1)
             found = True
-            end = time.perf_counter()
             break
         else:
             found = False
-
     if found == False:
         print(name, "not found")
-        end = time.perf_counter()
 
-    time_difference = end - start
-    times.append(time_difference)
-
-print(times)
-print(statistics.median(times))
-
-for i in Students:
+def hash_function(name):
     NameInt = 0
-    for j in range(len(i)):
-        NameInt = NameInt + ord(i[j])
-    StudentsInt.append(NameInt)
-
-for i in range(len(StudentsInt)):
-    StudentsIntHash.append(StudentsInt[i] % 19)
-
-for i in range(len(Students)):
-    HashTable[StudentsIntHash[i]].append(Students[i])
-
-times = []
-name = input("Enter a name: ")
-for j in range(100):
-    NameInt = 0
-    start = time.perf_counter()
     for i in range(len(name)):
         NameInt = NameInt + ord(name[i])
     NameInt = NameInt % 19
+    return NameInt
 
-    if name in HashTable[NameInt]:
-        print("Name Found at position", NameInt+1)
-        end = time.perf_counter()
+for name in Students:
+    index = hash_function(name)
+    HashTable[index].append(name)
+
+def hash_search(name):
+    if name in HashTable[hash_function(name)]:
+        print("Name Found at position", hash_function(name)+1)
     else:
         print("Name not found")
-        end = time.perf_counter()
 
-    time_difference = end - start
-    times.append(time_difference)
+name = input("input a name: ")
+start = time.perf_counter()
+linear_search(name)
+end = time.perf_counter()
+time_difference = end - start
+times.append(time_difference)
 print(times)
-print(statistics.median(times))
+
+times = []
+start = time.perf_counter()
+hash_search(name)
+end = time.perf_counter()
+time_difference = end - start
+times.append(time_difference)
+print(times)
