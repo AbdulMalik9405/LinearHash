872267a2339a753e0d5a5770474f6514bb80ffa5
AbdulMalik9405 <abdulmaliktojahmedov9405@gmail.com>
2025-09-19 08:48:00 +0900

    Added binary search function to go through an ordered student list. With time and number of trials calculations.

--- LinearHashing.py
+++ LinearHashing.py
@@ -6,16 +6,20 @@ StudentsHash = []
 HashTable = [[] for _ in range(20)]
 
 def linear_search(name):
+    count = 0
     found = True
     for i in range(len(Students)):
+        count = count + 1
         if Students[i] == name:
             print(name, " found at position", i+1)
             found = True
+            print(count, "Number of trials to find", name)
             break
         else:
             found = False
     if found == False:
         print(name, "not found")
+        print(count, "Number of trials")
 
 def hash_function(name):
     NameInt = 0
@@ -24,29 +28,24 @@ def hash_function(name):
     NameInt = NameInt % 19
     return NameInt
 
-for name in Students:
-    index = hash_function(name)
-    HashTable[index].append(name)
+for names in Students:
+    index = hash_function(names)
+    HashTable[index].append(names)
 
 def hash_search(name):
-    if name in HashTable[hash_function(name)]:
-        print("Name Found at position", hash_function(name)+1)
-    else:
+    count = 0
+    Flag = False
+    for i in range(len(HashTable[hash_function(name)])):
+        count = count + 1
+        if name == HashTable[hash_function(name)][i]:
+            print("Name Found at position", hash_function(name)+1)
+            print(count, "Number of trials to find", name)
+            Flag = True
+            break
+    if Flag == False:
         print("Name not found")
-
-name = input("input a name: ")
-start = time.perf_counter()
-linear_search(name)
-end = time.perf_counter()
-time_difference = end - start
-print(time_difference)
-
-times = []
-start = time.perf_counter()
-hash_search(name)
-end = time.perf_counter()
-time_difference = end - start
-print(time_difference)
+        print(count, "Number of trials")
+            
 
 def bubble_sort():
     flag = True
@@ -56,9 +55,6 @@ def bubble_sort():
             if Students[i] > Students[i+1]:
                 Students[i], Students[i+1] = Students[i+1], Students[i]
                 flag = True
-
-bubble_sort()
-print(Students)
 
 def insertion_sort():
     flag = True
@@ -71,5 +67,46 @@ def insertion_sort():
                     flag = True
                     break
 
-insertion_sort()
-print(Students)
+def binary_search(name):
+    count = 0
+    lower_bound = 0
+    upper_bound = len(Students)-1
+    middle = (upper_bound-lower_bound)//2
+    found = True
+    while found:
+        count = count + 1
+        if lower_bound > upper_bound:
+            print(name, "not found")
+            print(count, "Number of trials")
+            found = False
+        elif Students[middle] == name:
+            print(name, "found at position", middle + 1)
+            print(count, "Number of trials to find", name)
+            found = False
+        elif Students[middle] < name:
+            lower_bound = middle+1
+            middle = lower_bound + (upper_bound-lower_bound)//2
+        else:
+            upper_bound = middle-1
+            middle = upper_bound - (upper_bound-lower_bound)//2
+
+name = input("input a name: ")
+
+start = time.perf_counter()
+linear_search(name)
+end = time.perf_counter()
+time_difference = end - start
+print(time_difference)
+
+start = time.perf_counter()
+hash_search(name)
+end = time.perf_counter()
+time_difference = end - start
+print(time_difference)
+
+bubble_sort()
+start = time.perf_counter()
+binary_search(name)
+end = time.perf_counter()
+time_difference = end - start
+print(time_difference)
